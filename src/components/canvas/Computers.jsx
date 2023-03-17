fed7a931fd1ae479dc56d1eee54120a66c430a1e
sheuen <sheuenjaramillo@gmail.com>
2023-03-17 11:27:51 -0300

    Computers

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -14,8 +14,19 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <pointLight intensity={1} />
+      <spotLight 
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive 
         object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   )
@@ -30,17 +41,17 @@ const ComputersCanvas = () => {
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls 
+        <OrbitControls
           enableZoom={false} 
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
         <Computers />
-
-        <Preload all />
       </Suspense>
+
+      <Preload all />
     </Canvas>
   )
 }
 
-export default Computers
+export default ComputersCanvas
