d8abf80bea379ec4ce73fe9ec9adf7618c01df7a
sheuen <sheuenjaramillo@gmail.com>
2023-03-17 16:19:52 -0300

    About

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,15 +46,15 @@ const services = [
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "Javascript Developer",
     icon: mobile,
   },
   {
-    title: "Backend Developer",
+    title: "React Developer",
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Backend Developer",
     icon: creator,
   },
 ];
