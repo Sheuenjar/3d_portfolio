5d17e0157b319c60c95f7b9bec6942185d32fd35
sheuen <sheuenjaramillo@gmail.com>
2023-03-18 01:26:46 -0300

    Contact and Earth

--- src/constants/index.js
+++ src/constants/index.js
@@ -175,7 +175,7 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      "I thought it was impossible to make a website as beautiful as our product, but Sheuen proved me wrong.",
     name: "Sara Lee",
     designation: "CFO",
     company: "Acme Co",
@@ -183,7 +183,7 @@ const testimonials = [
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      "I've never met a web developer who truly cares about their clients' success like Sheuen does.",
     name: "Chris Brown",
     designation: "COO",
     company: "DEF Corp",
@@ -191,7 +191,7 @@ const testimonials = [
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      "After Sheuen optimized our website, our traffic increased by 50%. We can't thank them enough!",
     name: "Lisa Wang",
     designation: "CTO",
     company: "456 Enterprises",
